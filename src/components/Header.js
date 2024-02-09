c9217d878ccb14d949c8238af3cf74365865e970
Jonas Moons <moons_jonas@outlook.com>
2024-02-09 19:36:04 +0100

    add menu button to header and spaced all elements evenly

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,11 @@ import './Header.css'
 const Header = (props) => {
   return (
     <div className='Header'>
+      <div className='menu'>
+        <button>menu</button>
+      </div>
       <h1>🌗 Jonas Moons 🌓</h1>
+      <div className="spacer"></div>
     </div>
   )
 };
