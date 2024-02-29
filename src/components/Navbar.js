7bc407a4144e069377bbb2525c43a0554981d4b5
Jonas Moons <moons_jonas@outlook.com>
2024-02-29 19:34:53 +0100

    phone view triggers on 31.25em.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ import './Navbar.css'
 const Navbar = (props) => {
   return (
     <nav className='navbar'>
-      {window.innerWidth >= 500 && <div className="spacer">-</div>}
-      {window.innerWidth >= 500 ? <h1>🌗 Jonas Moons 🌓</h1> : <h1 className="phone-spacer">Jonas M</h1>}
+      {window.innerWidth >= 31.25 * parseFloat(getComputedStyle(document.documentElement).fontSize) && <div className="spacer">-</div>}
+      {window.innerWidth >= 31.25 * parseFloat(getComputedStyle(document.documentElement).fontSize) ? <h1>🌗 Jonas Moons 🌓</h1> : <h1 className="phone-spacer">Jonas M</h1>}
       <ul className='menu'> { props.children }</ul>
       <div className="spacer">-</div>
     </nav>
