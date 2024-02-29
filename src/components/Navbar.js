4238a68695ad5c1096754d49351e2de202bd96c8
Jonas Moons <moons_jonas@outlook.com>
2024-02-29 17:00:08 +0100

    small changes to navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = (props) => {
   return (
     <nav className='navbar'>
       {window.innerWidth >= 466 && <div className="spacer">-</div>}
-      {window.innerWidth >= 466 ? <h1>🌗 Jonas Moons 🌓</h1> : <h3 className="phone-spacer">Jonas M</h3>}
+      {window.innerWidth >= 466 ? <h1>🌗 Jonas Moons 🌓</h1> : <h1 className="phone-spacer">Jonas M</h1>}
       <ul className='menu'> { props.children }</ul>
       <div className="spacer">-</div>
     </nav>
