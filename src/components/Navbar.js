d3ca034223da6f3f28fbb94bbd49b2abc644c24d
Jonas Moons <moons_jonas@outlook.com>
2024-02-29 20:51:16 +0100

    small title change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = (props) => {
   return (
     <nav className='navbar'>
       {window.innerWidth >= 31.25 * parseFloat(getComputedStyle(document.documentElement).fontSize) && <div className="spacer">-</div>}
-      {window.innerWidth >= 31.25 * parseFloat(getComputedStyle(document.documentElement).fontSize) ? <h1>🌗 Jonas Moons 🌓</h1> : <h1 className="phone-spacer">Jonas M</h1>}
+      {window.innerWidth >= 31.25 * parseFloat(getComputedStyle(document.documentElement).fontSize) ? <h1>🌗 Jonas Moons 🌓</h1> : <h1 className="phone-spacer">🌗 Jonas Moons 🌓</h1>}
       <ul className='menu'> { props.children }</ul>
       <div className="spacer">-</div>
     </nav>
