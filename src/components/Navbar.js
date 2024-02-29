17d2345ba052153921b96c34d04348c95acefe1c
Jonas Moons <moons_jonas@outlook.com>
2024-02-29 17:02:59 +0100

    change size for phone view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ import './Navbar.css'
 const Navbar = (props) => {
   return (
     <nav className='navbar'>
-      {window.innerWidth >= 466 && <div className="spacer">-</div>}
-      {window.innerWidth >= 466 ? <h1>🌗 Jonas Moons 🌓</h1> : <h1 className="phone-spacer">Jonas M</h1>}
+      {window.innerWidth >= 500 && <div className="spacer">-</div>}
+      {window.innerWidth >= 500 ? <h1>🌗 Jonas Moons 🌓</h1> : <h1 className="phone-spacer">Jonas M</h1>}
       <ul className='menu'> { props.children }</ul>
       <div className="spacer">-</div>
     </nav>
