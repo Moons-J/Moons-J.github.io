314f7e8c95947c3fee584e63eb97662505e40094
Jonas Moons <moons_jonas@outlook.com>
2024-03-01 18:25:01 +0100

    adapt home page styling

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -4,9 +4,10 @@ import './HomeCard.css'
 const HomeCard = (props) => {
   return (
 
-    <div className='home-card basic-card flex-center'>
+    <div className='home-card basic-card flex-center align-column'>
       <h2>welcome</h2>
       <h3>to the Portfolio of Jonas Moons.</h3>
+      <p>Feel free to browse around and learn more about me and my work.</p>
       <a href="/aboutme">about me</a>
       <a href="/mywork">my work</a>
     </div>
