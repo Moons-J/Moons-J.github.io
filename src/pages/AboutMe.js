bfdd6eb66c47580b5b1432eb631ba5cd5efe1892
Jonas Moons <moons_jonas@outlook.com>
2024-03-06 17:00:39 +0100

    add about me card and main container

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,10 +1,15 @@
 import PersonalCard from "../components/PersonalCard";
+import AboutCard from "../components/AboutCard";
+import "./AboutMe.css";
 
 export default function AboutMe() {
   return (
     // main div horizontal
-    <div>
+    <div className="main-container">
       <PersonalCard />
+      <div>
+      <AboutCard />
+      </div>
     </div>
   );
 }
