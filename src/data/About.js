b92d63be0b7973b872fb972e6a8bde89ecbb1ed6
Jonas Moons <moons_jonas@outlook.com>
2024-03-06 17:09:40 +0100

    add first skills to data

---
+++ src/data/About.js
@@ -0,0 +1,20 @@
+const skills = [
+  {
+    id: 1,
+    img: "../data/data_icons/standard.svg",
+    title: "JavaScript, HTML, CSS",
+    origin: "Learned at Le Wagon.",
+    extra: "Used all over my projects.",
+    date: "10/2023"
+  },
+  {
+    id: 2,
+    img: "../data/data_icons/ruby.svg",
+    title: 'Ruby and Ruby on Rails',
+    origin: 'Learned at Le Wagon.',
+    extra: 'Used in My New Meal Plan.',
+    date: '10/2023'
+  },
+]
+
+export { skills }
