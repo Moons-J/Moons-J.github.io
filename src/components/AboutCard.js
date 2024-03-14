c14fff79b221bad2cb89a86156b27fdccf2fc0e1
Jonas Moons <moons_jonas@outlook.com>
2024-03-14 14:16:56 +0100

    add about card component

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,20 +1,51 @@
 import React from "react"
 import './AboutCard.css'
+import { skills } from "../data/About"
+
 
 const AboutCard = (props) => {
+  const date = new Date();
+  const today = date.toLocaleDateString();
+  const startDate = new Date(props.date);
+  const endDate = new Date(today);
+  const monthsDiff = (endDate.getFullYear() - startDate.getFullYear()) * 12 + (endDate.getMonth() - startDate.getMonth());
+
   return (
     <div className='about-card basic-card'>
+      <img className="card-img" src={props.img} alt={props.title + " image."} />
       <div>
-        <img className="card-img" src={props.img} alt={props.title + " image."} />
-        <div>
-          <h3>{props.title}</h3>
-          <p>{props.origin}</p>
-          <p>{props.extra}</p>
-          <p>{props.date}</p>
-        </div>
+        <h3>{props.title}</h3>
+        <p><u>Acquired:</u> {props.origin}</p>
+        <p><u>Used:</u> {props.extra}</p>
+        <p><u>From:</u> {props.date} <i>~ m/d/y ~</i></p>
+        <p><u>Experience:</u> {monthsDiff} months</p>
+        {/* <p><u>Last used: </u>{today}</p> */}
       </div>
     </div>
   )
 };
+function createCard(skills) {
+  return (
+    <AboutCard
+      key={skills.id}
+      img={skills.img}
+      title={skills.title}
+      origin={skills.origin}
+      extra={skills.extra}
+      date={skills.date}
+    />
+  );
+}
 
-export default AboutCard;
+function cardGenerator(props) {
+  return (
+    <div className="card-container">
+      {skills.map(createCard)}
+    </div>
+  );
+}
+
+
+export default cardGenerator;
+
+// "../data/data_icons/standard.svg"
