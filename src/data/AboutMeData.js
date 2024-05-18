e2480cc5c4165e0b5de0d37d047c3e506479dfae
Jonas Moons <moons_jonas@outlook.com>
2024-05-18 18:53:53 +0200

    fix naming

---
+++ src/data/AboutMeData.js
@@ -0,0 +1,93 @@
+// import React from 'react'
+// import { ReactComponent as MenuIcon } from ".";
+const aboutMeData = {
+  skills: [
+    {
+      id: 1,
+      img: require("../data/data_icons/standard.svg").default,
+      title: "JavaScript, HTML, CSS",
+      date: "10/09/2023",
+      extra: [
+        "Acquired: Le Wagon Barcelona.",
+        "Project: All projects.",
+        "From: Oct/09/2023"
+      ]
+    },
+    {
+      id: 2,
+      img: require("../data/data_icons/ruby.svg").default,
+      title: "Ruby and Ruby on Rails",
+      date: "10/09/2023",
+      extra: [
+        "Acquired: Le Wagon Barcelona.",
+        "Project: My New Meal Plan.",
+        "From: Oct/09/2023"
+      ]
+    },
+    {
+      id: 3,
+      img: require("../data/data_icons/node-js.svg").default,
+      title: 'Node JS',
+      date: '10/09/2023',
+      extra: [
+        'Acquired: Le Wagon Barcelona.',
+        'Project: Portfolio & My New Meal Plan.',
+        'From: Oct/09/2023',
+      ]
+    },
+    {
+      id: 4,
+      img: require("../data/data_icons/react.svg").default,
+      title: 'React',
+      date: '02/07/2024',
+      extra: [
+        'Acquired: Self Study.',
+        'Project: Portfolio.',
+        'From: Feb/07/2024',
+      ]
+    }
+  ],
+
+  hobbies: [
+    {
+      id: 1,
+      img: require('../data/data_icons/standard.svg').default,
+      title: '',
+      date: '',
+      extra: [
+        'Acquired: ',
+        'Project: ',
+        'From: ',
+      ]
+    },
+  ],
+
+  goals: [
+    {
+      id: 1,
+      img: require('../data/data_icons/standard.svg').default,
+      title: '',
+      date: '',
+      extra: [
+        'Acquired: ',
+        'Project: ',
+        'From: ',
+      ]
+    },
+  ],
+
+  extra: [
+    {
+      id: 1,
+      img:  require('../data/data_icons/standard.svg').default,
+      title: 'Test Extra',
+      date: '',
+      extra: [
+        'Acquired: ',
+        'Project: ',
+        'From: ',
+      ]
+    }
+  ]
+};
+export default aboutMeData ;
