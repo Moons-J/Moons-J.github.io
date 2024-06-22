c47eab96eae4133eaa9b22eb6ecfc8fe86cedf80
Jonas Moons <moons_jonas@outlook.com>
2024-06-22 12:52:03 +0200

    create basic BigCard component

---
+++ src/components/BigCard.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './BigCard.css'
+
+const BigCard = (props) => {
+  return (
+    <div className='big_card'>
+      <div className='title_box'></div>
+      <div className='skill_display'></div>
+      <div className='description_box'></div>
+    </div>
+  )
+};
+
+export default BigCard;
