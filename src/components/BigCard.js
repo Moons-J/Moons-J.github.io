5aad60b4707a29fd5ac9c5d9b535d1d3a6d8c7f4
Jonas Moons <moons_jonas@outlook.com>
2024-08-19 22:24:55 +0200

    create big card

--- src/components/BigCard.js
+++ src/components/BigCard.js
@@ -1,20 +1,30 @@
 import React from "react"
 import './BigCard.css'
+import Slider from "./Slider";
 
 const BigCard = (props) => {
   return (
-    <div className='big_card basic-card'>
-      <div className='title_box'>
-        <div className='skill_display'>
-          <h3>~ {props.title} ~</h3>
-          {props.top.map((item) => (
-            <h5>{item}</h5>
-          ))}
+    <div className='big-card basic-card'>
+      <div className='title-box'>
+        <div className='img-box'>
+          {props.img.length === 1 ? (
+            <img className="big-card-image" src={props.img[0]} alt={props.title} />
+          ) : (
+            <Slider images={props.img} />
+          )}
+          <div><h2 className="img-header">~ {props.title.toLowerCase()} ~</h2></div>
         </div>
       </div>
-      <div className='description_box'>
-        <p>{props.description}</p>
+      <div className='fav-display'>
+        <h3>Top 3 favorite parts:</h3>
+        {props.top.map((item) => (
+          <h5>{item}</h5>
+        ))}
       </div>
+      {/* <div className='desc-box'>
+        <h5>Info here.</h5>
+        <p>{props.description}</p>
+      </div> */}
     </div>
   )
 };
