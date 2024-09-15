a34984b9193fe92775e5c4bd4252949e62993a10
Jonas Moons <moons_jonas@outlook.com>
2024-09-15 16:07:56 +0200

    Reduce play speed of video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,29 @@
+import React, { useRef } from 'react';
 import HomeCard from "../components/HomeCard";
 import videoBG from "../data/data_video/bg.mp4";
 import "./Home.css";
 
 export default function Home() {
+  const videoRef = useRef();
+
+  const setPlayBack = () => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.7;
+    }
+  };
+
   return (
     <div>
     <div className="overlay"></div>
-      <video className="video-bg" src={videoBG} autoPlay loop muted />
+    <video
+        className="video-bg"
+        src={videoBG}
+        autoPlay
+        loop
+        muted
+        ref={videoRef}
+        onCanPlay={setPlayBack}
+      />
       <div className="Home flex-center container">
         <HomeCard />
       </div>
