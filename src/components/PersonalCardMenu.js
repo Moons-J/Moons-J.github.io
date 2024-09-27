278d65148a86df444cc162e8556d321287dbbf8d
Jonas Moons <moons_jonas@outlook.com>
2024-09-27 23:04:35 +0200

    fix personal menu highlighter

--- src/components/PersonalCardMenu.js
+++ src/components/PersonalCardMenu.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PersonalCardMenu = (props) => {
+  function clickFunction(event) {
+    props.buttonFunction(event);
+    event.target.classList.add('disabled');
+    const buttons = document.getElementsByClassName('about-button');
+    for (let i = 0; i < buttons.length; i++) {
+      if (buttons[i] !== event.target) {
+        buttons[i].classList.remove('disabled');
+      }
+    }
+  }
   return (
     <>
       <h2>menu</h2>
       {Object.keys(props.allData).map((key, index) => (
         <button
-        className={`about-button ${index}`}
-        onClick={props.buttonFunction}>{key.toString()}
+          key={index}
+          className={`about-button ${index}`}
+          onClick={clickFunction}
+        >{key.toString()}
         </button>
       ))}
     </>
